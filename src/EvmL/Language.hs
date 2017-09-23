cf25b489ff3909d3722f94c4b10c51f7f1228e77
Yiğit Özkavcı <yigitozkavci8@gmail.com>
2017-09-23 20:32:09 +0300

    Implement lexer

---
+++ src/EvmL/Language.hs
@@ -0,0 +1,28 @@
+module EvmL.Language where
+
+--------------------------------------------------------------------------------
+import           Text.Parsec.Language (emptyDef)
+import           Text.Parsec.Token
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+style :: LanguageDef state
+style =
+  emptyDef
+    { commentLine     = "--"
+    , reservedOpNames = ops
+    , reservedNames   = names
+    }
+
+ops :: [String]
+ops =
+  [ "+"
+  , "*"
+  , "-"
+  , "/"
+  , ";"
+  ]
+
+names :: [String]
+names =
+  ["if","if", "then", "else"]
---
+++ src/EvmL/Lexer.hs
@@ -0,0 +1,22 @@
+module EvmL.Lexer where
+
+--------------------------------------------------------------------------------
+import           Text.Parsec.Language (emptyDef)
+import           Text.Parsec.String   (Parser)
+import qualified Text.Parsec.Token    as Tok
+--------------------------------------------------------------------------------
+import           EvmL.Language        (style)
+--------------------------------------------------------------------------------
+
+lexer :: Tok.TokenParser ()
+lexer = Tok.makeTokenParser style
+
+integer    = Tok.integer lexer
+float      = Tok.float lexer
+parens     = Tok.parens lexer
+commaSep   = Tok.commaSep lexer
+semiSep    = Tok.semiSep lexer
+identifier = Tok.identifier lexer
+whitespace = Tok.whiteSpace lexer
+reserved   = Tok.reserved lexer
+reservedOp = Tok.reservedOp lexer
