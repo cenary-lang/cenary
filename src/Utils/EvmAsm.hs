b5fe3db9a03923b88a3c9d39f691667e44f9c0a0
Yiğit Özkavcı <yigitozkavci8@gmail.com>
2017-09-25 22:21:31 +0300

    Work on EVM assembler

--- src/Utils/EvmAsm.hs
+++ src/Utils/EvmAsm.hs
@@ -37,15 +37,28 @@ asm' (input:xs) =
     Right code' -> code' <> asm' xs
 
 toByteCode :: String -> Integer
+toByteCode "STOP" = 0x00
 toByteCode "ADD" = 0x01
+toByteCode "MUL" = 0x02
+toByteCode "SUB" = 0x03
+toByteCode "DIV" = 0x04
+toByteCode "POP" = 0x50
+toByteCode "MLOAD" = 0x51
+toByteCode "MSTORE" = 0x52
+toByteCode "JUMP" = 0x56
+toByteCode "JUMPI" = 0x57
+toByteCode "SWAP1" = 0x90
+toByteCode "DUP1" = 0x80
+toByteCode "JUMPDEST" = 0x5b
+toByteCode other = error $ "Instruction " <> other <> " is not recognised."
 
 toByteCode1 :: String -> Integer -> [Integer]
 toByteCode1 "PUSH" val = [0x60, val]
 
 instrParser :: Parser ByteCode
 instrParser = do
-  instr <- many upper
+  instr <- many alphaNum
   arg <- optionMaybe (space >> hexadecimal)
   case arg of
-    Just val -> return $ mconcat $ map (ByteCode . T.pack . printf "%x") $ toByteCode1 instr val
+    Just val -> return $ mconcat $ map (ByteCode . T.pack . printf "%02x") $ toByteCode1 instr val
     Nothing -> return $ ByteCode $ T.pack $ printf "%02x" $ toByteCode instr
