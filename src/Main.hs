93d188dc092faf1760eaf0c06012544ba3074375
Yiğit Özkavcı <yigitozkavci8@gmail.com>
2017-09-27 16:22:36 +0300

    Greatly improve error reporting

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ViewPatterns #-}
 
 module Main where
 
 ------------------------------------------------------
+import           Control.Applicative
 import           Control.Monad.State
+import Text.Parsec (ParseError)
 import           Control.Monad.Logger
 import           Control.Monad.Except
 import           Data.Monoid          ((<>))
@@ -12,6 +15,7 @@ import           Data.Text            as T
 import           Data.Text.IO         as T
 import           System.Environment
 import           System.Process
+import           System.IO (hClose)
 ------------------------------------------------------
 import qualified Ivy.Codegen         as C
 import           Ivy.Codegen.Types   (CodegenState (..), initCodegenState,
@@ -20,22 +24,39 @@ import qualified Ivy.Parser          as P
 import qualified Ivy.Syntax          as S
 import           Options              (Options (..), Mode (..), parseOptions, debug)
 import           Utils.EvmAsm        (asm)
+import Ivy.Codegen.Types (CodegenError(..))
 ------------------------------------------------------
 
-codegen :: Mode -> CodegenState -> [S.Expr] -> IO T.Text
+data Error =
+    Parsing ParseError
+  | Codegen CodegenError
+  | RuntimeError T.Text
+
+instance Show Error where
+  show (Parsing err) = "Parsing Error: " <> show err
+  show (Codegen err) = "Codegen Error: " <> show err
+  show (RuntimeError err) = "Codegen Error: " <> T.unpack err
+
+codegen :: Mode -> CodegenState -> [S.Expr] -> ExceptT Error IO T.Text
 codegen _ _ [] = return ""
 codegen mode state (e:ex) = do
-  result <- runExceptT (runStdoutLoggingT (execStateT (runEvm (C.codegenTop' e)) state))
+  result <- liftIO $ runExceptT (runStdoutLoggingT (execStateT (runEvm (C.codegenTop' e)) state))
   case result of
-    Left err       -> "" <$ (T.putStrLn "" >> print ("Codegen error: " <> show err))
+    Left (Codegen -> err)       -> throwError err
     Right newState ->
       (_byteCode newState <>) <$> codegen mode newState ex
 
-parse :: T.Text -> IO [S.Expr]
+parse :: T.Text -> ExceptT Error IO [S.Expr]
 parse code =
   case P.parseTopLevel code of
-    Left err -> error $ show err
+    Left (Parsing -> err) -> throwError err
     Right result -> return result
+
+execByteCode :: T.Text -> IO T.Text
+execByteCode byteCode = do
+  (_, Just hout, _, _) <- createProcess
+    (proc "evm" ["--debug", "--code", T.unpack byteCode, "run"]) { std_out = CreatePipe }
+  hGetContents hout <* hClose hout
 
 main :: IO ()
 main = do
@@ -44,24 +65,28 @@ main = do
   print inputFile
 
   code <- T.readFile inputFile
-  case mode of
-    Run -> do
-      ast <- parse code
-      codegen mode initCodegenState ast >>= execByteCode
-      print ast
-    Debug ->
-      void $ parse code >>= codegen mode initCodegenState
-    Asm -> do
-      let byteCode = asm (T.lines code)
-      print $ "Bytecode: " <> byteCode
-      execByteCode byteCode
-    Disasm -> do
-      parse code >>= codegen mode initCodegenState >>= T.writeFile "yis"
-      (_, Just hout, _, _) <- createProcess (proc "evm" ["disasm", "yis"]){ std_out = CreatePipe  }
-      T.putStrLn =<< hGetContents hout
-      callProcess "rm" ["yis"]
+  result <- runExceptT (go code mode)
+  case result of
+    Left err -> T.putStrLn "" >> T.putStrLn (T.pack (show err))
+    Right () -> return ()
   where
-    execByteCode :: T.Text -> IO ()
-    execByteCode byteCode = do
-      (_, Just hout, _, _) <- createProcess (proc "evm" ["--debug", "--code", T.unpack byteCode, "run"]){ std_out = CreatePipe }
-      T.putStrLn =<< hGetContents hout
+    go :: T.Text -> Mode -> ExceptT Error IO ()
+    go code mode = 
+      case mode of
+        Run -> do
+          ast <- parse code
+          codegen mode initCodegenState ast >>= liftIO . execByteCode >>= liftIO . T.putStrLn
+          liftIO $ print ast
+        Debug ->
+          void $ parse code >>= codegen mode initCodegenState
+        Asm -> do
+          let byteCode = asm (T.lines code)
+          liftIO $ print $ "Bytecode: " <> byteCode
+          liftIO $ T.putStrLn =<< execByteCode byteCode
+        Disasm -> do
+          parse code >>= codegen mode initCodegenState >>= liftIO . T.writeFile "yis"
+          (_, Just hout, _, _) <- liftIO $ createProcess (proc "evm" ["disasm", "yis"]){ std_out = CreatePipe  }
+          liftIO $ do
+            T.putStrLn =<< hGetContents hout
+            callProcess "rm" ["yis"]
+            hClose hout
