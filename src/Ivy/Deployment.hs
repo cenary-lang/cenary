541b05c74b48bf3968ea4163eeb6e3e788792e6f
Yiğit Özkavcı <yigitozkavci8@gmail.com>
2018-04-18 12:42:27 +0300

    Refactor deployment module

--- src/Ivy/Deployment.hs
+++ src/Ivy/Deployment.hs
@@ -1,6 +1,6 @@
 module Ivy.Deployment where
 
-import           Control.Monad.IO.Class
+import           Control.Applicative (liftA2)
 import           Data.Semigroup ((<>))
 import           System.Process hiding (env)
 
@@ -13,18 +13,16 @@ prepareDeployment
   -> m (bytecode, abi)
 prepareDeployment code parse toAbi toBytecode = do
   ast <- parse code
-  abi <- toAbi ast
-  bytecode <- toBytecode ast
-  pure (bytecode, abi)
+  liftA2 (,) (toBytecode ast) (toAbi ast)
 
 runDeployment
   :: String
   -> String
   -> IO ()
 runDeployment bytecode abi = do
-  liftIO $ callProcess "cp" ["deployment/deployment.js", "deployment/deployment.current.js"]
-  liftIO $ callProcess "sed" ["-i", "", "s/@bin@/" <> bytecode <> "/", "deployment/deployment.current.js"]
-  liftIO $ callProcess "sed" ["-i", "", "s/@abi@/" <> abi <> "/g", "deployment/deployment.current.js"]
+  callProcess "cp" ["deployment/deployment.js", "deployment/deployment.current.js"]
+  callProcess "sed" ["-i", "", "s/@bin@/" <> bytecode <> "/", "deployment/deployment.current.js"]
+  callProcess "sed" ["-i", "", "s/@abi@/" <> abi <> "/g", "deployment/deployment.current.js"]
 
 rewindDeployment :: IO ()
 rewindDeployment =
